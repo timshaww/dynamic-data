a54a49c8710a1b591e740ada8ca5eafa96554dc8
timshaww <time4shaw@gmail.com>
2025-02-18 12:03:13 -0500

    view and delete

--- miami/lib/handler.js
+++ miami/lib/handler.js
@@ -1,21 +1,43 @@
-let eList = require('../data/emails.json')
+let eList = require('../data/emails.json');
 
-const fs = require('fs')
+const fs = require('fs');
 
-exports.newsletterSignup = (req,res) => {
-    res.render('newsletter-signup', { csrf : 'supersecret'  })
- }
- 
-exports.newsletterSignupProcess = (req,res) => {
-    console.log(req.body)
-    eList.users.push(req.body)
-    console.log(eList)
-    let json = JSON.stringify(eList)
-    fs.writeFileSync('./data/emails.json', json, 'utf-8')
-    res.redirect(303, '/newsletter/list')
-}
+const nav = require('../data/navigation.json');
 
-exports.newsletterSignupList = (req,res) => {
-    console.log(eList)
-    res.render('userspage', {"users":eList.users})
-}
+exports.newsletterSignup = (req, res) => {
+	res.render('newsletter-signup', { csrf: 'supersecret' });
+};
+
+exports.newsletterSignupProcess = (req, res) => {
+	console.log(req.body);
+	eList.users.push(req.body);
+	console.log(eList);
+	let json = JSON.stringify(eList);
+	fs.writeFileSync('./data/emails.json', json, 'utf-8');
+	res.redirect(303, '/newsletter/list');
+};
+
+exports.newsletterSignupList = (req, res) => {
+	const eList = require('../data/emails.json');
+	console.log(eList);
+	res.render('userspage', { users: eList.users, nav });
+};
+
+exports.newsletterUser = (req, res) => {
+	console.log(eList);
+	let userDetails = eList.users.filter((user) => {
+		return user.email === req.params.email;
+	});
+	res.render('userdetails', { users: userDetails, nav });
+};
+
+exports.newsletterDelete = (req, res) => {
+	console.log(eList);
+	let newList = eList.users.filter((user) => {
+		return user.email !== req.params.email;
+	});
+	eList.users = newList;
+	let json = JSON.stringify(eList);
+	fs.writeFileSync('./data/emails.json', json, 'utf-8');
+	res.redirect(303, '/newsletter/list');
+};
--- miami/miami.js
+++ miami/miami.js
@@ -1,80 +1,78 @@
 // https://expressjs.com/
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 /*
 body-parser is a middleware for Node.js that processes the incoming request bodies before they reach the handlers. It is used to extract data from the request body and make it available in the req.body property. This is essential for handling data submitted through HTML forms and JSON data. 
 */
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:true}))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 // Import routes handlers
-const handler = require('./lib/handler')
+const handler = require('./lib/handler');
 
 //Setup the template engine handlebars
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 
-// To set the port execute: port=8080 node miami  
-const port = process.env.port || 3000
+// To set the port execute: port=8080 node miami
+const port = process.env.port || 3000;
 // Store navigation data
-let navigation = require("./data/navigation.json")
+let navigation = require('./data/navigation.json');
 //Create some routes
-app.get('/', (request,response)=>{
-    response.type("text/html")
-    response.render("home" , { title:"Miami Travel Site", nav: navigation  })
-})
+app.get('/', (request, response) => {
+	response.type('text/html');
+	response.render('home', { title: 'Miami Travel Site', nav: navigation });
+});
 
-app.get('/beaches', (request,response)=>{
-    response.type("text/html")
-    response.render("page", {title :"Miami Beaches",nav:navigation})
-})
+app.get('/beaches', (request, response) => {
+	response.type('text/html');
+	response.render('page', { title: 'Miami Beaches', nav: navigation });
+});
 
-app.get('/nightlife', (request,response)=>{
-    response.type("text/html")
-    response.render("page",{title:"Miami Night Life",nav:navigation})
-})
+app.get('/nightlife', (request, response) => {
+	response.type('text/html');
+	response.render('page', { title: 'Miami Night Life', nav: navigation });
+});
 
-app.get('/about', (request, response)=>{
-    response.type("text/html")
-    response.render("page",{title: "About Miami",nav:navigation})
-})
-// Query, params and body 
-app.get('/search', (request, response)=>{
-    console.log(request)
-    response.type("text/html")
-    response.render("page",{title: "Search results for: " + request.query.q,
-        nav:navigation
-    })
-})
+app.get('/about', (request, response) => {
+	response.type('text/html');
+	response.render('page', { title: 'About Miami', nav: navigation });
+});
+// Query, params and body
+app.get('/search', (request, response) => {
+	console.log(request);
+	response.type('text/html');
+	response.render('page', { title: 'Search results for: ' + request.query.q, nav: navigation });
+});
 //Basic GET form
-app.get('/basic',(req,res) => {
-    res.render('page', {req})
-})
+app.get('/basic', (req, res) => {
+	res.render('page', { req });
+});
 
 //Newsletter Routes
-app.get('/newsletter-signup', handler.newsletterSignup)
-app.post('/newsletter-signup/process', handler.newsletterSignupProcess)
-app.get('/newsletter/list', handler.newsletterSignupList)
-
+app.get('/newsletter-signup', handler.newsletterSignup);
+app.post('/newsletter-signup/process', handler.newsletterSignupProcess);
+app.get('/newsletter/list', handler.newsletterSignupList);
+app.get('/newsletter/detail/:email', handler.newsletterUser);
+app.get('/newsletter/delete/:email', handler.newsletterDelete);
 
 //error handling goes after the actual routes
 //The default response is not found
-app.use((request,response) => {
-    response.type("text/html")
-    response.status(404)
-    response.send("404 not found")
-})
+app.use((request, response) => {
+	response.type('text/html');
+	response.status(404);
+	response.send('404 not found');
+});
 //Server Error
-app.use ( (error, request,response,next)=>{
-    console.log(error)
-    response.type("text/html")
-    response.status(500)
-    response.send("500 server error") 
-})
+app.use((error, request, response, next) => {
+	console.log(error);
+	response.type('text/html');
+	response.status(500);
+	response.send('500 server error');
+});
 
 //start the server
-app.listen(port, ()=> {
-    console.log(`Express is running on http://localhost:${port};`)
-    console.log(`Press Ctrl-C to terminate.`)
-    })
-    
+app.listen(port, () => {
+	console.log(`Express is running on http://localhost:${port};`);
+	console.log(`Press Ctrl-C to terminate.`);
+});
